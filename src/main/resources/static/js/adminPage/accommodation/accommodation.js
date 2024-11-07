51c54429f335c8e943d8d6d17e75e153ef37d7b0
jinwoo <dlwlsdn8022@gmail.com>
2024-11-07 15:35:21 +0900

    이전 프로젝트에서 템플릿 사용으로 무분별한 jquery 파일들 정리하고 하나의 버전으로 통합하고 안쓰는버전의 파일들 정리

--- src/main/resources/static/js/adminPage/accommodation/accommodation.js
+++ src/main/resources/static/js/adminPage/accommodation/accommodation.js
@@ -1,35 +1,37 @@
-$(document).ready(function() {
-    // 폼 제출 이벤트 처리
-    $('#accommodationForm').on('submit', function(event) {
-        event.preventDefault(); // 폼 기본 제출 막기
+$(document).ready(
+    function() {
+        // 폼 제출 이벤트 처리
+        $('#accommodationForm').on('submit', function(event) {
+            event.preventDefault(); // 폼 기본 제출 막기
 
-        // 폼 데이터 가져오기
-        const postData = {
-            name: document.getElementById('name').value,
-            phone: document.getElementById('phone').value,
-            accommodationType: document.querySelector('input.accommodationType:checked').value,
-            // imagePath: document.getElementById('photo').files[0],
-            address1: document.getElementById('sample5_region').value,
-            address2: document.getElementById('sample5_roadName').value,
-            latitude: document.getElementById('sample5_lat').value,
-            longitude: document.getElementById('sample5_lon').value,
-            description: document.getElementById('editor').innerHTML
-        };
+            // 폼 데이터 가져오기
+            const postData = {
+                name: $('#name').val(),
+                phone: $('#phone').val(),
+                accommodationType: $('input.accommodationType:checked').val(),
+                // imagePath: $('#photo')[0].files[0],  // 파일 선택 시에는 [0]으로 접근
+                address1: $('#sample5_region').val(),
+                address2: $('#sample5_roadName').val(),
+                latitude: $('#sample5_lat').val(),
+                longitude: $('#sample5_lon').val(),
+                description: $('#editor').html()
+            };
 
-        // AJAX 요청 보내기 (JSON 형식)
-        $.ajax({
-            type: 'POST',
-            url: '/api/accommodation/create',
-            data: JSON.stringify(postData),
-            contentType: 'application/json',
-            success: function() {
-                alert('게시글이 성공적으로 등록되었습니다!');
-                window.location.href = '/index';
-            },
-            error: function(error) {
-                alert('게시글 등록에 실패했습니다.');
-                console.error("Error:", error);
-            }
+            // AJAX 요청 보내기 (JSON 형식)
+            $.ajax({
+                type: 'POST',
+                url: '/api/accommodation/create',
+                data: JSON.stringify(postData),
+                contentType: 'application/json',
+                success: function(res) {
+                    alert('게시글이 성공적으로 등록되었습니다!');
+                    const newId = res.id;
+                    window.location.href = '/accommodation/'+newId;
+                },
+                error: function(error) {
+                    alert('게시글 등록에 실패했습니다.');
+                    console.error("Error:", error);
+                }
+            });
         });
-    });
 });
